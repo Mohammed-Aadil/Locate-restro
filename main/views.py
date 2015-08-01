25d669b4efb872add568ccda81b2b59117b227c4
Dextor <mailtoaadilhanif@gmail.com>
2015-08-01 11:57:40 +0530

    models working

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -11,9 +12,8 @@ from main.serializer import DataSerializer
 # Create your views here.
 @api_view(['GET','POST'])
 def locate(request):
-    if(request.method=="GET"):
+    if(request.method=="GET" and User.objects.filter(username=request.user)):
         r=restro.objects.all()
         s=DataSerializer(r, many=True)
         return Response(s.data)
-    elif(request.method=="POST"):
-        return Response(status=status.HTTP_302_FOUND)
+    return Response(status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
